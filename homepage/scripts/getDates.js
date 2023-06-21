8dff4ba681056f3971e88cc32ab979d94d43a78b
Brittany Ridley <britt.ridley@outlook.com>
2023-06-20 22:20:23 -0500

    counter

--- homepage/scripts/getDates.js
+++ homepage/scripts/getDates.js
@@ -25,25 +25,18 @@ function closeMenu() {
 }
 
 // Visitor counter
-var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
-var visitCount = localStorage.getItem("page_view");
+const countElement = document.getElementById('count');
 
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
+update();
+
+function update() {
+	fetch('https://api.countapi.xyz/update/samcode/pen/?amount=1').then(res => res.json()).then(res => {
+		countElement.innerHTML = res.value;
+	})
 }
-counterContainer.innerHTML = visitCount;
 
-// Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-});
+
+
+
 
 
