05a57b2310f6afeb1032c381d88af59d784bd8da
Brittany Ridley <britt.ridley@outlook.com>
2023-06-20 23:03:45 -0500

    change file name to chamber.html

--- homepage/scripts/getDates.js
+++ homepage/scripts/getDates.js
@@ -24,16 +24,10 @@ function closeMenu() {
     navMenu.classList.remove("active");
 }
 
-// Visitor counter
-const countElement = document.getElementById('count');
 
-update();
 
-function update() {
-	fetch('https://api.countapi.xyz/update/samcode/pen/?amount=1').then(res => res.json()).then(res => {
-		countElement.innerHTML = res.value;
-	})
-}
+
+
 
 
 
